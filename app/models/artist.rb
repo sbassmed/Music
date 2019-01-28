e25acd932ac57057d34c2ff6f9d07ebb8d0cd9e9
sbassmed <sebastiancamilomarin@gmail.com>
2019-01-28 18:08:41 -0500

    Crea la migración

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  image_url  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Artist < ApplicationRecord
   has_many :albums
 end
--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id          :integer          not null, primary key
+#  artist_id   :integer
+#  name        :string
+#  image_url   :string
+#  released_at :date
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
